624110b10c13067c006f07291df79fd45310c02a
Jagannath Ghosh <ghoshjagannath0000@gmail.com>
2022-08-07 21:19:51 +0530

    script.py added for file

--- script.py
+++ script.py
@@ -1,13 +1,16 @@
 import sqlite3
-from flask import Flask,redirect,url_for,render_template,request,abort
+from flask import Flask,redirect,url_for,render_template,request,abort,flash
 from werkzeug.utils import secure_filename
 from werkzeug.datastructures import  FileStorage
+import os 
+
 
 #initialize flask web app 
 app = Flask(__name__)
 
+app.config['UPLOAD_FOLDER']=r"C:/Users/ghosh/AppData/Local/Programs/Python/Python39/Flask_project/static/files/"
 
-conn=sqlite3.connect(r'C:\Users\ghosh\AppData\Local\Programs\Python\Python39\Flask_project\password.db')
+conn=sqlite3.connect(r'C:/Users/ghosh/AppData/Local/Programs/Python/Python39/Flask_project/password.db')
 
 
 #creating user and password database in sqlite3 system if password.db is not present in parents folder. 
@@ -18,7 +21,7 @@ command='''CREATE TABLE  if not Exists user_list (name TEXT NOT NULL,pass TEXT N
 #telling us about the creator
 @app.route("/")
 def login():
-   return render_template('upload.html')
+   return render_template('upload_file.html')
  
  
 
@@ -28,23 +31,40 @@ def detail():
    if request.method=='POST':
       name=request.form['Uname']
       password=request.form['Pass']
-      conn=sqlite3.connect(r'C:\Users\ghosh\AppData\Local\Programs\Python\Python39\Flask_project\password.db')
-      c=conn.cursor()
-      c.execute("insert into user_list(name,pass) values(?,?)",('human','steel'))
-      conn.commit()
-      conn.close()
+      # conn=sqlite3.connect(r'C:/Users/ghosh/AppData/Local/Programs/Python/Python39/Flask_project/password.db')
+      # c=conn.cursor()
+      # c.execute("insert into user_list(name,pass) values(?,?)",('human','steel'))
+      # conn.commit()
+      # conn.close()
       return render_template('result.html',user=name,password=password)
+   else:
+      flash('You are doing something wrong')
 
 
 @app.route('/show') #this thing is not working  it is redirecting to the detail  page only 
 def show():
    while true:
-      conn=sqlite3.connect(r'C:\Users\ghosh\AppData\Local\Programs\Python\Python39\Flask_project\password.db')
+      conn=sqlite3.connect(r'C:/Users/ghosh/AppData/Local/Programs/Python/Python39/Flask_project/password.db')
       cur=conn.cursor()
       command='Select * from user_list'
       cur.execute(command)
       datas=cur.fetchall()
       return render_template("display.html",datas=datas)
+   
+   
+   
+   
+
+
+@app.route('/upload', methods = ['GET', 'POST'])
+#this thing is not working  it is redirecting to the detail  page only 
+def upload():
+   if request.method=='POST':
+      f=request.files['file']
+      f.save(secure_filename(f.filename))
+      return "very successful"
+   
+
 
 
 
