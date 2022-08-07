ceba45d7a130f4edb515e8cc96be3ce6929b0746
Jagannath Ghosh <ghoshjagannath0000@gmail.com>
2022-08-07 21:19:18 +0530

    password.db file updated

--- script.py
+++ script.py
@@ -1,6 +1,7 @@
-
 import sqlite3
-from flask import Flask,redirect,url_for,render_template,request
+from flask import Flask,redirect,url_for,render_template,request,abort
+from werkzeug.utils import secure_filename
+from werkzeug.datastructures import  FileStorage
 
 #initialize flask web app 
 app = Flask(__name__)
@@ -17,10 +18,9 @@ command='''CREATE TABLE  if not Exists user_list (name TEXT NOT NULL,pass TEXT N
 #telling us about the creator
 @app.route("/")
 def login():
-   return render_template('welcome.html')
+   return render_template('upload.html')
  
  
-
 
 #from log in page we are taking name and password  and storeing them in database (sqlite3)
 @app.route('/detail',methods=["POST","GET"])
@@ -34,6 +34,18 @@ def detail():
       conn.commit()
       conn.close()
       return render_template('result.html',user=name,password=password)
+
+
+@app.route('/show') #this thing is not working  it is redirecting to the detail  page only 
+def show():
+   while true:
+      conn=sqlite3.connect(r'C:\Users\ghosh\AppData\Local\Programs\Python\Python39\Flask_project\password.db')
+      cur=conn.cursor()
+      command='Select * from user_list'
+      cur.execute(command)
+      datas=cur.fetchall()
+      return render_template("display.html",datas=datas)
+
 
 
 if __name__=="__main__":
