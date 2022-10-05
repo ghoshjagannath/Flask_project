4d7fd248d0d71ba7f1033c687cad71da25a4c404
Jagannath Ghosh <ghoshjagannath0000@gmail.com>
2022-10-05 19:56:19 +0530

    Some file added and deleted and modified here

--- script.py
+++ script.py
@@ -1,90 +1,53 @@
 import sqlite3
 import os 
-from flask import Flask,redirect,url_for,render_template,request,abort,flash
+
+#basic level of module in flask
+from flask import Flask,redirect,url_for,render_template,request,abort,flash,session
+from flask_session import Session
+
+# for wtf froms to be used
 from flask_wtf import FlaskForm
 from wtforms import StringField,SubmitField
 from wtforms.validators import DataRequired
+
+
+#for file upload related  modules 
 from werkzeug.utils import secure_filename
 from formsubmit import *
 
+
+#for security of password it is used
+from werkzeug.security import generate_password_hash,check_password_hash
+
 #initialize flask web app 
 app = Flask(__name__)
-
 app.config['UPLOAD_FOLDER']=r"C:/Users/ghosh/AppData/Local/Programs/Python/Python39/Flask_project/static/files/"
-
-# conn=sqlite3.connect(r'C:/Users/ghosh/AppData/Local/Programs/Python/Python39/Flask_project/password.db')
-
-
-#creating user and password database in sqlite3 system if password.db is not present in parents folder. 
-
-# command='''CREATE TABLE  if not Exists user_list (name TEXT NOT NULL,pass TEXT NOT NULL)'''
-
 app.config['SECRET_KEY']='1234'
 
+#####################################
+# conn=sqlite3.connect(r'C:/Users/ghosh/AppData/Local/Programs/Python/Python39/Flask_project/password.db')
+#creating user and password database in sqlite3 system if password.db is not present in parents folder. 
+# command='''CREATE TABLE  if not Exists user_list (name TEXT NOT NULL,pass TEXT NOT NULL)'''
 
-#telling us about the creator
-# @app.route("/")
-# def login():
-#    flash('You are successful')
-#    return render_template('upload_file.html')
+######################################
+
+
+
+
+# telling us about the creator
+#in this page we need to log in to the system 
+@app.route("/")
+def login():
+   flash('You are successful')
+   return render_template('login_page.html')
+
+
+@app.route('/success',methods=['GET'])
+def success():
+   session['name']=request.form.get('username')
+   data=session.pop('name')
  
  
-
-#from log in page we are taking name and password  and storeing them in database (sqlite3)
-@app.route('/detail',methods=["POST","GET"])
-def detail():
-   if request.method=='POST':
-      name=request.form['Uname']
-      password=request.form['Pass']
-      # conn=sqlite3.connect(r'C:/Users/ghosh/AppData/Local/Programs/Python/Python39/Flask_project/password.db')
-      # c=conn.cursor()
-      # c.execute("insert into user_list(name,pass) values(?,?)",('human','steel'))
-      # conn.commit()
-      # conn.close()
-      return render_template('welcome_result.html',user=name,password=password)
-   else:
-      flash('You are doing something wrong')
-
-
-@app.route('/show') #this thing is not working  it is redirecting to the detail  page only 
-def show():
-   while true:
-      conn=sqlite3.connect(r'C:/Users/ghosh/AppData/Local/Programs/Python/Python39/Flask_project/password.db')
-      cur=conn.cursor()
-      command='Select * from user_list'
-      cur.execute(command)
-      datas=cur.fetchall()
-      return render_template("display.html",datas=datas)
-   
-   
-   
-   
-
-
-@app.route('/upload', methods = ['GET', 'POST'])
-#this thing is not working  it is redirecting to the detail  page only 
-def upload():
-   if request.method=='POST':
-      f=request.files['file']
-      f.save(secure_filename(f.filename))
-      return render_template("upload_result.html",filename=f.filename)
-
-
-@app.route("/base")
-def base():
-   return render_template('base.html')
-
- 
-
-
-@app.route('/', methods=["GET",'POST'])
-def submit():
-    form = Form()
-    if form.validate_on_submit():
-       return redirect(url_for("/welcome.html"))
-    return render_template('wtf.html', form=form)
- 
-
 
 
 if __name__=="__main__":
